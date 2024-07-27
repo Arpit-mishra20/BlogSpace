b9be270b1fcbe414d5b1b3b5bffe33d30852c95b
Arpit-mishra20 <arpit.202033@gmail.com>
2024-07-27 12:58:21 +0530

    Configure openssl

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const Connection = async (username, password) => {
-    // const URL = `mongodb://localhost:27017`;
+    const URL =`mongodb+srv://arpit202033:codeforpractice@blog-app.g01fyj6.mongodb.net/blogApp?retryWrites=true&w=majority&appName=blog-app` ;
     
     try {
         await mongoose.connect(URL, { useNewUrlParser: true })
--- server/utils/upload.js
+++ server/utils/upload.js
@@ -1,13 +1,13 @@
 import multer from 'multer';
-import mongoose from 'mongoose';
-import Connection from '../../database/db.js';
+// import mongoose from 'mongoose';
+// import Connection from '../../database/db.js';
 import { GridFsStorage } from 'multer-gridfs-storage';
 // import db from '../../database/db.js';
 // import db from '../database/db.js';
 // const db = require('./database/db.js'); 
 
 const storage = new GridFsStorage({
-    url: `mongodb://${Process.env.username}:${Process.env.password}@ac-np7yr7j-shard-00-00.g01fyj6.mongodb.net:27017,ac-np7yr7j-shard-00-01.g01fyj6.mongodb.net:27017,ac-np7yr7j-shard-00-02.g01fyj6.mongodb.net:27017/?ssl=true&replicaSet=atlas-xnpc0j-shard-0&authSource=admin&retryWrites=true&w=majority&appName=blog-app`,
+    url: `mongodb://user2003:codeforpractice@ac-np7yr7j-shard-00-00.g01fyj6.mongodb.net:27017,ac-np7yr7j-shard-00-01.g01fyj6.mongodb.net:27017,ac-np7yr7j-shard-00-02.g01fyj6.mongodb.net:27017/?replicaSet=atlas-xnpc0j-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=blog-app`,
     options: { useNewUrlParser: true },
     file: (request, file) => {
         const match = ["image/png", "image/jpg"];
